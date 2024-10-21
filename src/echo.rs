fe49c42ea8349c68b54c5f3bf4dd994e44f02f96
Goodmorningpeople <zachy2020p@gmail.com>
2024-10-21 17:19:35 +0800

    removed debugging print statements

--- src/echo.rs
+++ src/echo.rs
@@ -5,13 +5,10 @@ pub fn match_echo(echo_args: Option<&ArgMatches>) {
         Some(args) => {
             if let Some(s) = args.get_one::<String>("string-input") {
                 if echo_args.unwrap().get_flag("newline-option") {
-                    println!("newline option");
                     print!(r"{}", s);
                 } else if echo_args.unwrap().get_flag("enable-special-option") {
-                    println!("special option");
                     println!("{}", s);
                 } else {
-                    println!("default option");
                     println!(r"{}", s);
                 }
             }
